202e2b441a20c6c437a0d893d7f44c49f4b56397
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-11-03 14:36:18 +0800

    有索引的时候，就只会锁住复合条件的行了

--- ch20/09--rowlockMethod.sql
+++ ch20/09--rowlockMethod.sql
@@ -46,3 +46,40 @@ SELECT * FROM tab_no_index
 #关闭事务
 SET AUTOCOMMIT=1;
 
+####
+#----------------有索引的时候，就只会锁住复合条件的行了
+###
+#创建带索引的表 tab_with_index
+DROP TABLE IF EXISTS tab_with_index;
+CREATE TABLE tab_with_index LIKE tab_no_index;
+ALTER TABLE tab_with_index
+	ADD INDEX idx(id);
+INSERT INTO tab_with_index
+	SELECT * FROM tab_no_index;
+SELECT * FROM tab_with_index;
+
+
+####
+#会话 session_1 加锁复合条件的指定行
+###
+#打开事务
+SET AUTOCOMMIT=0;
+
+#给指定行加排他锁
+SELECT * FROM tab_with_index
+	WHERE id=1
+	FOR UPDATE;
+
+#关闭事务
+SET AUTOCOMMIT=1;
+
+####
+#会话 session_2 可以加锁其他未被加锁的行
+###
+SET AUTOCOMMIT=0;
+SELECT * FROM tab_with_index
+	WHERE id=2
+	FOR UPDATE;
+
+#关闭事务
+SET AUTOCOMMIT=1;
