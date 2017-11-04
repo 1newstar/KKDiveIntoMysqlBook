ba7dc5868be1fe486560c04c511426d005156a89
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-11-04 09:26:23 +0800

    配置文件创建和指定索引缓存

--- ch21/01--optMyISAMMemo.sql
+++ ch21/01--optMyISAMMemo.sql
@@ -17,3 +17,23 @@ SET GLOBAL key_buffer_size=0;
 
 #显示警告
 SHOW WARNINGS;
+
+#设置指定表的索引缓存
+CACHE INDEX payment_myisam IN hot_cache;
+
+
+####
+#--------------配置文件创建和指定索引缓存
+###
+#自定义的热表索引缓存
+hot_cache.key_buffer_size=2G
+#自定义的冷表索引缓存
+cold_cache.key_buffer_size=1G
+#设置索引缓存初始化sql文件
+init_file=/mysqld_init.sql
+
+#mysqld_init.sql 中加载和指定缓存
+CACHE INDEX sales1 IN hot_cache;
+CACHE INDEX sales2 IN cold_cache;
+LOAD INDEX INTO CACHE sales1,sales2;
+
