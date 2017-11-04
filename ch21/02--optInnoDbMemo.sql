a837906c6a8f6a7eea82f0c16783639c61ecf3a4
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-11-04 10:08:44 +0800

    优化InnoDB存储的内存

--- ch21/02--optInnoDbMemo.sql
+++ ch21/02--optInnoDbMemo.sql
@@ -5,3 +5,18 @@
 #配置文件修改 InnoDB 缓存池的大小
 ###
 innodb_buffer_pool_size=8M
+
+
+#查看旧子列表的比例
+#——》默认37%
+SHOW GLOBAL VARIABLES LIKE '%innodb_old_blocks_pct%';
+
+#配置文件修改 InnoDB 的旧子表超时时间
+innodb_old_blocks_time=1000
+
+#配置文件修改 InnoDB 缓存池的实例数量
+innodb_buffer_pool_instances=8
+
+#查看双写状态
+#——》默认是开启的
+SHOW GLOBAL VARIABLES LIKE '%doublewrite%';
